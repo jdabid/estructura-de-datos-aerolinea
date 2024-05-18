c20c9212c8f3290511ee941e0ce2a34e48dfb853
David Castro <dabid.banegas@gmail.com>
2024-05-18 07:01:20 -0500

    commit 2

--- program-copy.js
+++ program-copy.js
@@ -4,10 +4,8 @@ const readlineSync = require('readline-sync');
 class Vuelo {
     #origen = '';
     #destino = '';
-    #impuestoDestino = 0;
     #costo = 0;
     #mascota = 1;
-    #promocion = 0;
 
     set origen(nuevoOrigen){
         this.#origen = nuevoOrigen;
@@ -21,13 +19,6 @@ class Vuelo {
     }
     get destino(){ 
         return this.#destino;
-    }
-
-    set impuestoDestino(nuevoImpuestoDestino){
-        this.#impuestoDestino = nuevoImpuestoDestino;
-    }
-    get impuestoDestino(){
-        return this.#impuestoDestino;
     }
 
     set costo(nuevoCosto){
@@ -44,33 +35,20 @@ class Vuelo {
         return this.#mascota;
     }
 
-    set promocion(nuevaPromocion){
-        this.#promocion = nuevaPromocion;
-    }
-    get promocion(){
-        return this.#promocion;
-    }
-
-    constructor(origen, destino, impuestoDestino, costo, mascota, promocion){
+    constructor(origen, destino, costo, mascota){
         if(origen == undefined){
             throw new Error('El origen del vuelo es requerido');}
         if(destino == undefined){
             throw new Error('El destino del vuelo es requerido');}
-        if(impuestoDestino == undefined){
-            throw new Error('El impuesto del destino es requerido');}
         if(costo == undefined){
             throw new Error('El costo del vuelo es requerido');}
         if(mascota == undefined){
             throw new Error('El campo mascota es requerido');}
-        if(promocion == undefined){
-            throw new Error('El campo promocion es requerido');}
 
         this.#origen = origen;
         this.#destino = destino;
-        this.#impuestoDestino = impuestoDestino;
         this.#costo = costo;
         this.#mascota = mascota;
-        this.#promocion = promocion;
 
     }
 }
@@ -107,9 +85,8 @@ class ListaVuelos {
             let i = 1;
             while(nodoTmp != null){
                 console.log(`Datos del vuelo numero ${i}`);
-                console.log(`origen: ${nodoTmp.valor.origen}:`);
                 console.log(`destino: ${nodoTmp.valor.destino}:`);
-                console.log(`impuestoDestino: ${nodoTmp.valor.impuestoDestino}:`);
+                console.log(`origen: ${nodoTmp.valor.origen}:`);
                 console.log(`costo: ${nodoTmp.valor.costo}:`);
                 nodoTmp = nodoTmp.siguiente;
                 i++;
@@ -129,7 +106,7 @@ class ListaVuelos {
             let i = 1;
             while(nodoTmp != null){
                 if(nodoTmp.valor.mascota == 1){
-                    nodoTmp.valor.impuestoSiMascota = nodoTmp.valor.costo * 0.1;
+                    nodoTmp.valor.impuestoSiMascota = 50000;
                     console.log(`El impuesto mascota es: ${nodoTmp.valor.impuestoSiMascota}`);
                     impuestoSiMascota += nodoTmp.valor.impuestoSiMascota;
                 }
@@ -147,43 +124,10 @@ class ListaVuelos {
         }
     }
 
-    impuestoPromocion(){
-        let impuestoSiPromocion = 0;
-        let totalImpuestoPromocion = 0;
-
-        if(this.cabeza == null){
-            console.log(`No hay vuelos para mostrar, no hay nodos en la lista`);
-        }
-        else {
-            let nodoTmp = this.cabeza;
-            let i = 1;
-            while(nodoTmp != null){
-                if(nodoTmp.valor.promocion == 1){
-                    nodoTmp.valor.impuestoSiPromocion = (nodoTmp.valor.costo* 0.1);
-                    nodoTmp.valor.costo = nodoTmp.valor.costo - nodoTmp.valor.impuestoSiPromocion;
-                    console.log(`El impuesto con promocion es: ${nodoTmp.valor.impuestoSiPromocion}`);
-                    console.log(`El costo del vuelo con promocion es: ${nodoTmp.valor.costo}`);
-                    impuestoSiPromocion += nodoTmp.valor.impuestoSiPromocion;
-                }
-                else {
-                    console.log(`El vuelo no tiene promocion`);
-                    totalImpuestoPromocion += nodoTmp.valor.costo + nodoTmp.valor.totalImpuestoPromocion;
-                }
-
-                nodoTmp = nodoTmp.siguiente;
-                i++;
-            }totalImpuestoPromocion += impuestoSiPromocion;
-            console.log(`${totalImpuestoPromocion}`);
-            console.dir(`**********************************************************`);
-            
-        }
-    }
     
 
     
-
-    
-    costoDelVuelo(){
+    nominaTotlaItems(){
         if(this.cabeza == null){
             console.log(`No hay vuelos para mostrar, no hay nodos en la lista`);
         }
@@ -192,11 +136,11 @@ class ListaVuelos {
             let i = 1;
             let totalCosto = 0;
             while(nodoTmp != null){
-                totalCosto += nodoTmp.valor.costo + nodoTmp.valor.impuestoDestino;
+                totalCosto += nodoTmp.valor.costo;
                 nodoTmp = nodoTmp.siguiente;
                 i++;
             }
-            console.log(`Costo + impuesto destino ${totalCosto}`);
+            console.log(`${totalCosto}`);
         }
     }
     
@@ -212,13 +156,11 @@ let continuar = true;
 while(continuar){
     const origen = readlineSync.question('****** Ingrese el origen del vuelo: ');
     const destino = readlineSync.question('Ingrese el destino del vuelo: ');
-    const impuestoDestino = +readlineSync.question('Ingrese el impuesto del destino: ');
     const costo = +readlineSync.question('Ingrese el costo del vuelo: ');
-    const mascota = +readlineSync.question('El vuelo incluye mascota? (s(1)/n(2)): ');
-    const promocion = +readlineSync.question('El vuelo tiene promocion? (s(1)/n(2)): ***************** ');
+    const mascota = +readlineSync.question('El vuelo incluye mascota? (s(1)/n(2)): ***************** ');
     
 
-    const vuelo = new Vuelo(destino, origen, impuestoDestino, costo, mascota, promocion);
+    const vuelo = new Vuelo(destino, origen, costo, mascota);
     listaVuelos.insertar(vuelo);
 
     const respuesta = readlineSync.question('Desea agregar otro vuelo? (s/n): ');
@@ -229,5 +171,4 @@ while(continuar){
 
 listaVuelos.mostrarTodosLosVuelos();
 listaVuelos.impuestoMascota();
-listaVuelos.costoDelVuelo();
-listaVuelos.impuestoPromocion();
+
